8c4af14fae22182599bd44a9feb074a54ab7df80
Ayush Yadav <aayush0029@gmail.com>
2024-09-17 15:31:06 +0530

    First commit

---
+++ PostBlog.Core/DBEntities/UsersAyush.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PostBlog.Core.DBEntities
+{
+    public class UsersAyush
+    {
+        [Key]
+        public int userId { get; set; } 
+        public string userName { get; set; }
+        public string password { get; set; }
+    }
+}
---
+++ PostBlog.Core/InboxContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PostBlog.Core
+{
+    public class InboxContext(DbContextOptions<InboxContext> options) : DbContext(options)
+    {
+
+    }
+}
